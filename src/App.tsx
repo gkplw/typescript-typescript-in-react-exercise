5b9257e40a479e4f2de2f0a7c50250a6e7c99406
Ponlawat <p.sangtub@gmail.com>
2025-08-25 16:57:02 +0700

    feat: add TypeScript types for props and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import "./App.css";
+import Greeting from "./Greeting";
 
 function App() {
   // TODO4 : Render ออกมาใน App.tsx
-  return;
+  return(
+    <>
+    <Greeting name="Alice" age={25} />
+    </>
+  )
 }
 
 export default App;
